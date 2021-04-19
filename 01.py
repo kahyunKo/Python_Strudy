872bc556b0a256a2ed4e98e086d0c6233050b969
kahyunKo <78471342+kahyunKo@users.noreply.github.com>
2021-04-20 01:25:04 +0900

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,27 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> message = "Hello Python world!"
+>>> print(message)
+Hello Python world!
+>>> message
+'Hello Python world!'
+>>> message = "Hello Python Crash Course world!"
+>>> message
+'Hello Python Crash Course world!'
+>>> simple_message = "message"
+>>> print(simple_message)
+message
+>>> 
+>>> name = "ada lovelace"
+>>> print(name.title())
+Ada Lovelace
+>>> print(name.upper())
+ADA LOVELACE
+>>> print(name.lower())
+ada lovelace
+>>> first_name = "ada"
+>>> last_name = "lovelace"
+>>> full_name = first_name + " " + last_name
+>>> print(full_name)
+ada lovelace
+>>> 
