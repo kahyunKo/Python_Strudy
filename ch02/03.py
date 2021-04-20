15209f5343f9fb2d32a30642ab91cfd2ce803f51
kahyunKo <bufar1014@daum.net>
2021-04-20 15:34:01 +0900

    Add files via upload

---
+++ ch02/03.py
@@ -0,0 +1,33 @@
+# 2-3
+name = "Eric"
+greeting = "안녕하세요"
+study = "오늘은 파이썬을 좀 배워볼까요?"
+s1 = greeting + " " + name + " " + study
+print(s1)
+print("\n")
+
+# 2-4
+name = "kokahyun"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("\n")
+
+# 2-5
+name = "알버트 아인슈타인"
+sentence = '"결코 실수하지 않은 사람은 아무것도 시도하지 않은 사람이다."'
+print(name + "은 이렇게 말했습니다.\n" + sentence + "\n")
+
+# 2-6
+famous_person = "알버트 아인슈타인"
+message = '"결코 실수하지 않은 사람은 아무것도 시도하지 않은 사람이다."'
+sentence = famous_person + "은 이렇게 말했습니다. \n" + message
+print(sentence)
+
+# 2-7 **
+name = " Ko kahyun "
+print("\t" + name + "\n")
+print("\t" + name.lstrip() + "\n")
+print("\t" + name.rstrip() + "\n")
+print("\t" + name.strip() + "\n")
+
